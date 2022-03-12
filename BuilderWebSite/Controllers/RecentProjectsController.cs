510820ba282fb19f2f83280ad174b7d5144aab56
Ekrem Alp Özmen <ekrem3792@gmail.com>
2022-03-12 19:41:44 +0300

    completed RecentProjectsPage

---
+++ BuilderWebSite/Controllers/RecentProjectsController.cs
@@ -0,0 +1,25 @@
+﻿using BuilderWebSite.Services.WebSite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuilderWebSite.Controllers
+{
+    public class RecentProjectsController : BaseController
+    {
+        private readonly RecentProjectsService _recentProjectsService;
+
+        public RecentProjectsController(RecentProjectsService recentProjectsService)
+        {
+            _recentProjectsService = recentProjectsService;
+        }
+        // GET: RecentProjects
+        public ActionResult Index()
+        {
+            var model = _recentProjectsService.GetRecentProjectsListIQueryable().ToList();
+            return View(model);
+        }
+    }
+}
